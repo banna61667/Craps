a2258f55b68e34ae2b29aab3c0261dff08d03586
Ikenna <ikscout15@gmail.com>
2020-01-29 11:43:16 -0800

    My entry

---
+++ Die.java
@@ -0,0 +1,18 @@
+public class Die{
+
+	
+
+
+	public int dieRollOne(){
+		int n1 = (int) ((Math.random()*6)+1);
+		return n1;
+	}
+
+	public int dieRollTwo(){
+		int n2 = (int) ((Math.random()*6)+1);
+		return n2;
+	}
+
+
+
+}
